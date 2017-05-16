1a18b4e453e25fe11bf21ea13349a6408f71e3b2
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-16 15:05:19 -0700

    WhackAMole
    WhackAMole Added!

--- Software Development Fundamentals/Week 1/src/WhackAMole/WhackAMole.java
+++ Software Development Fundamentals/Week 1/src/WhackAMole/WhackAMole.java
@@ -12,6 +12,30 @@ public class WhackAMole {
     /**
      * @param args
      */
+    private int score, molesLeft,attempts;
+    private char[][] moleGrid;
+    
+    WhackAMole(int numAttempts, int gridDimension){
+        this.attempts = numAttempts;
+        
+    }
+    
+    public boolean place(int x, int y){
+        return true;
+    }
+    
+    void Whack(int x, int y){
+        
+    }
+    
+    void printGridToUser(){
+        
+    }
+    
+    void printGrid(){
+        
+    }
+    
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
